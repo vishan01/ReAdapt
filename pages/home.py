95d37fd20dc393e388bede6f13d827e4db70c57f
Gudipati Vishnu Vardhan Reddy <70902772+vishan01@users.noreply.github.com>
2025-06-27 23:33:40 +0530

    Add file uploader and job info handling to home page

--- pages/home.py
+++ pages/home.py
@@ -1,3 +1,8 @@
 import streamlit as st
 st.set_page_config("Home")
 st.title("_ReAdapt_: Adapt :blue[Resume] as per :orange[Job]")
+uploaded_file=st.file_uploader("Upload Your Resume")
+if 'job_info' not in st.session_state:
+    st.session_state['job_info']=None
+if uploaded_file:
+    st.selectbox("Select Job Description/Link",["URL","TEXT"],placeholder="Choose your option")
