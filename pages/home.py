774bdb7b23852063b5d805193e6594de6e628878
Gudipati Vishnu Vardhan Reddy <70902772+vishan01@users.noreply.github.com>
2025-06-28 20:50:03 +0530

    Fix job info handling in home page: store selection in session state

--- pages/home.py
+++ pages/home.py
@@ -2,7 +2,14 @@ import streamlit as st
 st.set_page_config("Home")
 st.title("_ReAdapt_: Adapt :blue[Resume] as per :orange[Job]")
 uploaded_file=st.file_uploader("Upload Your Resume")
+
 if 'job_info' not in st.session_state:
     st.session_state['job_info']=None
+
 if uploaded_file:
-    st.selectbox("Select Job Description/Link",["URL","TEXT"],placeholder="Choose your option")
+    st.session_state['job_info']=st.selectbox("Select Job Description/Link",["URL","TEXT"],placeholder="Choose your option")
+
+if st.session_state['job_info']=="URL":
+    st.text_input("Enter the URL of the job")
+else:
+    st.text_area("Enter the Job Description")
