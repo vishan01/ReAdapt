f8b38dc0b05cf75898e5d73ce8600d57c98e6f5b
Gudipati Vishnu Vardhan Reddy <70902772+vishan01@users.noreply.github.com>
2025-06-30 19:04:21 +0530

    Refactor job info handling in home page: store job type and description in session state

--- pages/home.py
+++ pages/home.py
@@ -4,12 +4,13 @@ st.title("_ReAdapt_: Adapt :blue[Resume] as per :orange[Job]")
 uploaded_file=st.file_uploader("Upload Your Resume")
 
 if 'job_info' not in st.session_state:
-    st.session_state['job_info']=None
+    st.session_state['job_info']={}
 
 if uploaded_file:
-    st.session_state['job_info']=st.selectbox("Select Job Description/Link",["URL","TEXT"],placeholder="Choose your option")
+    st.session_state['job_info']['type']=st.selectbox("Select Job Description/Link",["URL","TEXT"],placeholder="Choose your option")
 
 if st.session_state['job_info']=="URL":
-    st.text_input("Enter the URL of the job")
+    job_url=st.text_input("Enter the URL of the job")
 else:
-    st.text_area("Enter the Job Description")
+    job_desc=st.text_area("Enter the Job Description")
+    st.session_state['job_info']['job_desc']=job_desc
