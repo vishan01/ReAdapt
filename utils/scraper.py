91f1c1bc65089a30864d497967de19075191226b
Gudipati Vishnu Vardhan Reddy <70902772+vishan01@users.noreply.github.com>
2025-07-12 21:42:16 +0530

    Update dependencies in pyproject.toml and uv.lock: add langchain-core, langchain-google-genai, and adjust cachetools version

--- utils/scraper.py
+++ utils/scraper.py
@@ -1,23 +1,20 @@
-import bs4
-from langchain_community.document_loaders import WebBaseLoader
-import asyncio 
+from pydantic import BaseModel
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.prompts import PromptTemplate
 
-def retrieve_data(page_url:str)->list[str]:
-    """
-    Retrieve the page data from the given url
 
-    params:
-        page_url: URL of the job description
+class JobInfo(BaseModel):
+    job_title: str
+    job_skills: str
+    job_description: str
 
-    return:
-        parsed data of the page
-    """
-    
-    loader = WebBaseLoader(web_paths=[page_url])
-    docs = []
-    for doc in loader.load():
-        docs.append(doc)
+parser = JsonOutputParser(pydantic_object=JobInfo)
 
-    doc = docs[0]
-    return docs
+prompt = PromptTemplate.from_template("""
+Extract the job information from the text below and respond in the specified JSON format.
 
+{format_instructions}
+
+Job Posting:
+{job_posting}
+""")
